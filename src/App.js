1ed51fc29b66c4662df13f15265e7ecb71e2acaa
myfwong <fred.wong.sn@gmail.com>
2020-01-05 03:17:05 +0000

    feature

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          I am the King!  HAHAHA
         </a>
       </header>
       <p className="App-intro">
